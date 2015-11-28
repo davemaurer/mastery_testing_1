48142c1bb9665d1198c9b6aeae65649e457f93e3
David Maurer <davemaurermail@gmail.com>
2015-11-28 16:21:07 -0700

    login and logout now work, and added confirmation for password, which I forgot.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,17 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       flash[:notice] = "Nice Job, You're up and running!"
       redirect_to root_path
     else
       flash[:notice] = "Invalid login. Please try again"
       render :new
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :password, presence: true, confirmation: true
+  validates_confirmation_of :password
 end
