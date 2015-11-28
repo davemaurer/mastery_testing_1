f07bddbbd6e439f29ad1fe40a300b056cde661b2
David Maurer <davemaurermail@gmail.com>
2015-11-28 15:18:42 -0700

    setup redirect to new on unsuccessful login but no flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Nice Job, You're up and running!"
+      redirect_to root
+    else
+      flash[:notice] = "Invalid login. Please try again"
+      render :new
+    end
   end
 end
