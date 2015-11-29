1d012a676d5affcda69dff303cbd2440ce9108fb
David Maurer <davemaurermail@gmail.com>
2015-11-28 17:48:46 -0700

    minor styling with foundation and added new link form

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,16 +1,30 @@
 class LinksController < ApplicationController
   def index
+    @link = Link.new
   end
 
   def new
   end
 
   def create
+    link = Link.new(link_params)
+    if link.save
+      redirect_to links_path
+    else
+      flash[:alert] = "URL invalid. Please try again."
+      redirect_to links_path
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:title, :url)
   end
 end
